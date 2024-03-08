2bea6ddca6d659725e72fde07b066c571a965292
Alex Koiter <akoiter@gmail.com>
2024-03-08 17:01:58 -0600

    Included search by coords and created functions

--- Quarter section locations/Quarter section search.R
+++ Quarter section locations/Quarter section search.R
@@ -17,16 +17,83 @@ MB_corners <- read.csv("MB_QuarterSectionCorners.csv") %>%
          M = str_sub(DLS_KEY, start = 1, end = 1)) %>%
   mutate(legal = paste(paste(Q, S,`T`, R, sep = "-"), M, sep ="")) 
 
-head(MB_corners)
 
-# Map two Quarters for context
-quarters <- c("NE-11-33-29W", "NE-1-33-29W")  #Do not include any leading zeros
+## Search by coordinates 
+closest_centoid <- function(X, Y, data = MB_corners) {
+  data %>%
+    bind_cols(X = X, Y = Y) %>%
+    mutate(dist = (X - EASTING)^2 + (NORTHING - Y) ^2) %>%
+    filter(dist == min(dist)) %>%
+    select(-X, -Y)
+}
 
-search <- MB_corners %>%
-  filter(legal %in% quarters) %>%
-  st_as_sf(coords = c("EASTING", "NORTHING"), crs = "+proj=utm +zone=14 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+search_coord <- function(long, lat) {
+  tibble(long = long, lat = lat) %>%
+    st_as_sf(coords = c("long", "lat"), crs = "+proj=longlat +datum=WGS84") %>%
+    st_transform(crs = "+proj=utm +zone=14 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")  %>%
+    bind_cols(st_coordinates(.)) %>%
+    st_drop_geometry() %>%
+    rowid_to_column("point") %>%
+    mutate(dist = map2(X, Y, closest_centoid)) %>%
+    unnest(dist) %>%
+    select(point, legal) %>%
+    left_join(tibble(long = long, lat = lat) %>%
+                rowid_to_column("point"), by = "point") %>%
+    select(-point)
+}
 
-m <- mapview(search, map.type = "Esri.WorldImagery")
-m
+## Search by legal designation
+search_legal <- function(x, data = MB_corners) {
+  data %>%
+    filter(legal %in% x) %>%
+    st_as_sf(coords = c("EASTING", "NORTHING"), crs = "+proj=utm +zone=14 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") %>%
+    st_transform(crs = "+proj=longlat +datum=WGS84") %>%
+    bind_cols(st_coordinates(.)) %>%
+    st_drop_geometry() %>%
+    select(legal, long = X, lat = Y) %>%
+    tibble()
+}
+
+## Mapping Functions
+centroid <- function(x, data = MB_corners) {
+  data %>%
+    right_join(x, by = join_by(legal)) %>%
+    st_as_sf(coords = c("EASTING", "NORTHING"), crs = "+proj=utm +zone=14 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") %>%
+    st_transform(crs = "+proj=longlat +datum=WGS84") %>%
+    select(legal, geometry)
+}
+
+polygon <- function(x, data = MB_corners) {
+  data %>%
+    right_join(x, by = join_by(legal)) %>%
+    pivot_longer(cols = NE_E:SE_N, names_to = c("corner", "coord"), names_sep = "_", values_to = "value") %>%
+    pivot_wider(names_from = coord, values_from = value) %>%
+    drop_na(N) %>%
+    st_as_sf(coords = c("E", "N"), crs = "+proj=utm +zone=14 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") %>%
+    st_transform(crs = "+proj=longlat +datum=WGS84") %>%
+    group_by(legal) %>%
+    summarise() %>%
+    ungroup() %>%  
+    st_convex_hull()
+}
 
 
+map_quarter <- function(x, map.type = "Esri.WorldImagery") {
+  Polygon <- polygon(x)
+  Centroid <- centroid(x)
+    mapview(list(Centroid, Polygon), map.type = map.type, homebutton = FALSE)
+}            
+
+
+## Examples of the three functions
+test1 <- search_coord(long = c(-101.4656, -102.4660), lat = c(51.81913, 51.83500))
+test1
+
+test2 <- search_legal(x = c("NE-11-33-29W", "SW-20-2-1W"))
+test2
+
+test3 <- map_quarter(test1)
+test3
+
+test4 <-map_quarter(test2)
+test4
--- Quarter section locations/README.md
+++ Quarter section locations/README.md
@@ -1,5 +1,19 @@
 # Quarter Section Locations
 
-A short little R script to locate Quarter section coordinates in Manitoba by legal land designation name, e.g., NW-31-5-27W.
+A short little R script that includes three functions:
+
+1) Locate Quarter section coordinates in Manitoba by legal land designation name (e.g., NW-31-5-27W). 
+  - `search_legal()` 
+2) Locate Quarter section in Manitoba by longitude and latitude, e.g., -101.4656, 51.81913.
+  - `search_coord()`
+  - Locates quarter section based on the shortest euclidean distance to the center of the section
+3) Map the above centroids and ploygons of the located sections 
+  - `map_quarter()`
+
+Still a work in progress. My to do list:
+1) Include checks in `search_coord()`. Right now it will **always** return the closest matching quarter section, even if coordinates are outside MB
+2) Turn this into an R package!!!
+  - Need a clever name (open to suggestions)
+3) Have the package peer-reviewed
 
 Data source: https://mli.gov.mb.ca/quarter_sec/index.html
